a3947dbc8914d40a0e42853c0d6874a2beb8a5b8
kenn <libin887180@163.com>
2017-08-22 08:37:45 +0800

    增加statelayout

--- app/src/main/java/com/zhongdi/miluo/ui/activity/ModifyDealPswActivity.java
+++ app/src/main/java/com/zhongdi/miluo/ui/activity/ModifyDealPswActivity.java
@@ -1,15 +1,11 @@
 package com.zhongdi.miluo.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.zhongdi.miluo.R;
 import com.zhongdi.miluo.base.BaseActivity;
 import com.zhongdi.miluo.presenter.ModifyDealPswPresenter;
 import com.zhongdi.miluo.view.ModifyDealPswView;
-
-import butterknife.OnClick;
 
 public class ModifyDealPswActivity extends BaseActivity<ModifyDealPswPresenter> implements ModifyDealPswView {
 
@@ -27,16 +23,5 @@ public class ModifyDealPswActivity extends BaseActivity<ModifyDealPswPresenter> 
     @Override
     protected void initialize() {
 
-    }
-
-    @OnClick({R.id.rl_bank_card, R.id.rl_safe_center})
-    public void onViewClicked(View view) {
-        switch (view.getId()) {
-            case R.id.rl_bank_card:
-                break;
-            case R.id.rl_safe_center:
-                startActivity(new Intent(mContext,SafeCenterActivity.class));
-                break;
-        }
     }
 }
