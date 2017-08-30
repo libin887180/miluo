b65b2a3755c80ae9627e84fedeb372cbe757c751
libin <libin887180@163com>
2017-08-30 19:48:25 +0800

    ChartView

---
+++ app/src/main/java/com/zhongdi/miluo/view/mpchart/MyLineChart.java
@@ -0,0 +1,49 @@
+package com.zhongdi.miluo.view.mpchart;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.github.mikephil.charting.charts.BarLineChartBase;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.interfaces.dataprovider.LineDataProvider;
+import com.github.mikephil.charting.renderer.LineChartRenderer;
+
+/**
+ * Created by libin on 2017/8/30.
+ */
+
+public class MyLineChart extends BarLineChartBase<LineData> implements LineDataProvider {
+
+    public MyLineChart(Context context) {
+        super(context);
+    }
+
+    public MyLineChart(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyLineChart(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        mRenderer = new MyLineChartRenderer(this, mAnimator, mViewPortHandler);
+    }
+
+    @Override
+    public LineData getLineData() {
+        return mData;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        // releases the bitmap in the renderer to avoid oom error
+        if (mRenderer != null && mRenderer instanceof LineChartRenderer) {
+            ((LineChartRenderer) mRenderer).releaseBitmap();
+        }
+        super.onDetachedFromWindow();
+    }
+}
