a788da53f6522defb470c3b788b605f8934c6c81
kenn <libin887180@163.com>
2017-09-02 14:58:16 +0800

    基金详情布局

---
+++ app/src/main/java/com/zhongdi/miluo/adapter/market/SellPremiumAdapter.java
@@ -0,0 +1,80 @@
+package com.zhongdi.miluo.adapter.market;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.zhongdi.miluo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by liuyu on 2017/2/11.
+ */
+
+public class SellPremiumAdapter extends BaseAdapter {
+    private Context mContext;
+    private List<String> dataList = new ArrayList<>();
+
+    public SellPremiumAdapter(Context context) {
+        this.mContext = context;
+    }
+
+    public void setDataList(List<String> dataList) {
+        this.dataList = dataList;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getCount() {
+
+        return dataList.size();
+    }
+
+    @Override
+    public String getItem(int position) {
+        return dataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.item_premium, null);
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.tvDec.setText(dataList.get(position));
+//        holder.tvDepRate.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG); //中划线
+//        holder.tvDepRate.getPaint().setFlags(Paint. STRIKE_THRU_TEXT_FLAG|Paint.ANTI_ALIAS_FLAG);  // 设置中划线并加清晰
+        holder.tvDepRate.setText("");
+        return convertView;
+    }
+
+
+    static class ViewHolder {
+        @BindView(R.id.tv_dec)
+        TextView tvDec;
+        @BindView(R.id.tv_dep_rate)
+        TextView tvDepRate;
+        @BindView(R.id.tv_rate)
+        TextView tvRate;
+
+        ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+}
--- app/src/main/java/com/zhongdi/miluo/ui/activity/market/PremiumActivity.java
+++ app/src/main/java/com/zhongdi/miluo/ui/activity/market/PremiumActivity.java
@@ -3,13 +3,33 @@ package com.zhongdi.miluo.ui.activity.market;
 import android.os.Bundle;
 
 import com.zhongdi.miluo.R;
+import com.zhongdi.miluo.adapter.market.BuyPremiumAdapter;
+import com.zhongdi.miluo.adapter.market.OperationPremiumAdapter;
+import com.zhongdi.miluo.adapter.market.SellPremiumAdapter;
 import com.zhongdi.miluo.base.BaseActivity;
 import com.zhongdi.miluo.presenter.PremiumPresenter;
 import com.zhongdi.miluo.view.PremiumView;
+import com.zhongdi.miluo.widget.NOScollListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
 
 public class PremiumActivity extends BaseActivity<PremiumPresenter> implements PremiumView {
 
 
+    @BindView(R.id.lv_apply)
+    NOScollListView lvApply;
+    @BindView(R.id.lv_redeem)
+    NOScollListView lvRedeem;
+    @BindView(R.id.lv_operations)
+    NOScollListView lvOperations;
+
+
+    private BuyPremiumAdapter premiumAdapter;
+    private SellPremiumAdapter sellPremiumAdapter;
+    private OperationPremiumAdapter operationPremiumAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +43,35 @@ public class PremiumActivity extends BaseActivity<PremiumPresenter> implements P
 
     @Override
     protected void initialize() {
+
+        premiumAdapter = new BuyPremiumAdapter(mContext);
+        lvApply.setAdapter(premiumAdapter);
+        List<String> strings = new ArrayList<>();
+        strings.add("111");
+        strings.add("222");
+        strings.add("333");
+        strings.add("444");
+        premiumAdapter.setDataList(strings);
+
+
+        sellPremiumAdapter = new SellPremiumAdapter(mContext);
+        lvRedeem.setAdapter(sellPremiumAdapter);
+        List<String> redems = new ArrayList<>();
+        redems.add("111");
+        redems.add("222");
+        redems.add("333");
+        redems.add("444");
+        sellPremiumAdapter.setDataList(redems);
+
+
+        operationPremiumAdapter = new OperationPremiumAdapter(mContext);
+        lvOperations.setAdapter(operationPremiumAdapter);
+        List<String> operations = new ArrayList<>();
+        operations.add("111");
+        operations.add("222");
+        operations.add("333");
+        operations.add("444");
+        operationPremiumAdapter.setDataList(operations);
     }
 
 }
